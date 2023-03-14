a653c7125bc167545c3baf7d78a4a21b1a328f96
Julio Cesar de Souza <julios100305@gmail.com>
2023-03-13 21:26:58 -0300

    commit inicial

---
+++ TesteEF/Data/TesteEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TesteEF.Models.Departments;
+
+namespace TesteEF.Data
+{
+    public class TesteEFContext : DbContext
+    {
+        public TesteEFContext (DbContextOptions<TesteEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TesteEF.Models.Departments.Departments> Departments { get; set; } = default!;
+    }
+}
---
+++ TesteEF/Models/Departments/Departments.cs
@@ -0,0 +1,13 @@
+﻿namespace TesteEF.Models.Departments
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Departments(int id,string name )
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
