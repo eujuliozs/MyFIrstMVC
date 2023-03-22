438c3c4372b79db0b64b8b6f623038fc752da9af
Julio Cesar de Souza <julios100305@gmail.com>
2023-03-22 14:37:28 -0300

    FK solution

--- TesteEF/Models/Service/SellerService.cs
+++ TesteEF/Models/Service/SellerService.cs
@@ -15,6 +15,7 @@ namespace TesteEF.Models.Service
         }
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
